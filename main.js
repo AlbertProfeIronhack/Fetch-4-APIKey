c8b39d6bcb4fb8aa9a1f773ceaac12b52df3bd30
AlbertProfeIronhack <157632155+AlbertProfeIronhack@users.noreply.github.com>
2025-08-21 20:08:57 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// conexion api del tiempo con api key
+
+const urlTiempo = "https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=12bcd931813b66fb794cd92941110130&units=metric&lang=es";
+
+const ubicacion = document.getElementById("ubicacion");
+const icono = document.getElementById("icono");
+const general = document.getElementById("general");
+const temperatura = document.getElementById("temperatura");
+const humedad = document.getElementById("humedad");
+
+fetch(urlTiempo)
+.then(res=>res.json())
+.then((data)=>{
+    console.log(data);
+    // ubicacion, general, icono, temperatura, humedad
+    console.log(data.name);
+    console.log(data.weather[0].main);
+    console.log(data.weather[0].icon);
+    console.log(data.main.temp);
+    console.log(data.main.humidity);
+    let icon = data.weather[0].icon
+
+    //******** visualmente */
+    ubicacion.innerText = data.name;
+    icono.innerHTML = `<img src= https://openweathermap.org/img/wn/${icon}.png>`;
+    general.innerText = data.weather[0].main;
+    temperatura.innerText = `${data.main.temp} ºC`;
+    humedad.innerText = `${data.main.humidity} %`;
+
+})
+.catch((error)=>{
+    console.error(error);
+});
